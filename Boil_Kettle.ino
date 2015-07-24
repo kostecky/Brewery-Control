37a95c98c288a193aa4bdf4174e20abe10097a4d
Kristian Kostecky <kris@vmfarms.com>
2015-07-24 19:28:58 -0400

    Adjust boundary conditions to avoid unnecessary switching. Add some error bars on changes in POT resistance to stabilize the circuit

--- Boil_Kettle.ino
+++ Boil_Kettle.ino
@@ -10,7 +10,7 @@
 int POT_PIN = 2; // select the input pin for the potentiometer
 int SSR_PIN = 13; // select the pin for SSR - also the inbuilt LED
 int CYCLE_LENGTH = 2000; // How many miliseconds is the full duty cycle
-int CYCLE_RESOLUTION = 50; // Check the potentiometer every 50ms to see if we need to adjust the duty cycle
+int CYCLE_RESOLUTION = 100; // Check the potentiometer every 50ms to see if we need to adjust the duty cycle
 int POT_MAX = 1023; // Max reading from the pot
 int POT_MIN = 0; // Min reading from the pot
 
@@ -34,7 +34,12 @@ void loop() {
   pot_val = analogRead(POT_PIN);
 
   // If we complete a duty cycle or the potentiometer changes, we start another cycle
-  if ((cycle_time_left <= 0) || (pot_val != last_pot_val)) {
+  if ((cycle_time_left <= 0) || 
+      ((pot_val != last_pot_val) && ((pot_val+1 < last_pot_val) || (pot_val-1 > last_pot_val)) )) {
+    Serial.print("New cycle: time_left: ");
+    Serial.println(cycle_time_left);
+    Serial.print("pot_val: ");
+    Serial.println(pot_val);
     last_pot_val = pot_val;
     
     percent_time_on = float(pot_val)/float(POT_MAX);    
@@ -43,9 +48,12 @@ void loop() {
     cycle_time_left = CYCLE_LENGTH;
     
     // Ignite coil!
-    digitalWrite(SSR_PIN, HIGH);    
+    if (pot_val > POT_MIN) {
+      digitalWrite(SSR_PIN, HIGH);    
+    }
   } else {
-    if (cycle_time_left <= time_to_switch_state) {
+    if ((cycle_time_left <= time_to_switch_state) && (pot_val < POT_MAX)) {
+      // Extinguish coil
       digitalWrite(SSR_PIN, LOW);
     }
     cycle_time_left -= CYCLE_RESOLUTION;
